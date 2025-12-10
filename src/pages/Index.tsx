f0f307971b9fdffb5deb6b43ba370f74c62dbf9c
Seu Nome <gabrielmoraesbastos2112@gmail.com>
2025-12-10 18:52:50 -0300

    Fotos

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -9,27 +9,28 @@ import artisanImage from "@/assets/artisan-portrait.jpg";
 import productWood from "@/assets/product-wood.jpg";
 import productCrochet from "@/assets/product-crochet.jpg";
 import productCeramic from "@/assets/product-ceramic.jpg";
+import productAlls from "@/assets/product-alls.jpg"
 
 const featuredProducts = [
   {
     id: 1,
-    name: "Kit especial 10 Cestas Clássicas Média",
+    name: "Kit especiaPalha de Pinus para embalagens Pacote com 40g.l 10 Cestas Clássicas Média",
     category: "Madeira",
-    price: "R$ 120,00",
-    image: productWood,
+    price: "R$ 19,00",
+    image: productAlls,
   },
   {
     id: 2,
-    name: "Manta em Crochê Artesanal",
-    category: "Crochê",
-    price: "R$ 320,00",
+    name: "Cesta Clássica Pequena em Vime Natural",
+    category: "Madeira",
+    price: "R$ 12,00",
     image: productCrochet,
   },
   {
     id: 3,
-    name: "Vaso Decorativo em Cerâmica",
-    category: "Cerâmica",
-    price: "R$ 250,00",
+    name: "50 hastes de vime enrolado para arranjos / Rococó",
+    category: "Madeira",
+    price: "R$ 19,30 - 30,90",
     image: productCeramic,
   },
 ];
